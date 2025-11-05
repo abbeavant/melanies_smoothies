c7816511af06d46a1da32c3d0ed85fc314a05a64
abbeavant <elizabeth.avant@us.abb.com>
2025-11-05 08:35:17 -0600

    Display fruit options in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ st.write('The name on your smoothie will be', name_on_order)
 #Bring it all together Lesson 1: 🥋 Display the Fruit Options List in Your Streamlit in Snowflake (SiS) App. 
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: '
