9d082b2b0371acf5d710f78c96bc8b97c1009a36
abbeavant <elizabeth.avant@us.abb.com>
2025-11-05 09:26:52 -0600

    Enable dataframe display for Pandas conversion
    Uncommented dataframe display and stop commands for the Pandas dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,13 +23,13 @@ st.write('The name on your smoothie will be', name_on_order)
 #Bring it all together Lesson 1: 🥋 Display the Fruit Options List in Your Streamlit in Snowflake (SiS) App. 
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
 #Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: '
